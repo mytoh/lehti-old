05413ff8fa64d3b0699d8e7f711b5d2f63a1ada1
mytoh <mytoh@gmail.com>
2012-10-17 22:04:21 +0900

    add bin

---
+++ install.scm
@@ -0,0 +1,29 @@
+(use file.util)
+(use gauche.process)
+
+(define *lehti-directory*
+  (if (sys-getenv "LEHTI_DIR")
+    (sys-getenv "LEHTI_DIR")
+    (build-path (home-directory)
+                ".lehti")))
+
+(define install
+  (lambda ()
+    (print "cloning repository")
+    (run-process `(git clone git://github.com/mytoh/lehti ,*lehti-directory*) :wait #t)
+
+    (print "creating riisi directory")
+
+    (print "make executable")
+    (sys-chmod (build-path *lehti-directory* "bin/leh") #o755)
+
+    (newline)
+    ))
+
+
+(define (main args)
+  (cond
+    ((file-exists? *lehti-directory*)
+     (print "please remove %s directory " (sys-getenv "LEHTI_DIR")))
+    (else
+      (install))))
